f12ae6abfb6a274ab43e64cab606028114a351a8
Gabriele Fariello <gabriele.fariello@gmail.com>
2023-07-23 17:38:58 -0400

    Cleaned up code and added module docstring for general/polyfile.py

--- general/polyfile.py
+++ general/polyfile.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python3
+"""
+polyfile - A Python module for versatile file handling.
 
+This module provides utility classes and functions for reading files of various formats
+(plain text, gzip, bz2, zst) and from various sources (local files, HTTP/HTTPS URLs,
+FTP, SSH, and SFTP servers).
+
+Key Classes:
+- `FTPSessionWrapper`: A wrapper around `ftplib.FTP` for more flexible FTP session handling.
+- `PolyReader`: A class designed for reading files line-by-line with support for various
+   compression formats and remote sources.
+
+Usage:
+To read from a local gzipped file:
+with PolyReader("path/to/localfile.gz") as reader:
+    for line in reader:
+        process(line)
+
+To read from a remote HTTP source:
+with PolyReader("http://example.com/data.txt") as reader:
+    for line in reader:
+        process(line)
+
+Dependencies:
+- `paramiko`: For SSH/SFTP support.
+- `zstandard`: For zst file support.
+- `requests`: For HTTP/HTTPS support.
+- `ftputil`: For enhanced FTP utilities.
+- `tqdm`: For progress bars.
+
+Author: Gabriele Fariello
+Version: 1.0.0
+"""
 import argparse
 import io
 import bz2
@@ -7,7 +39,7 @@ import gzip
 import paramiko
 import zstandard as zstd
 import requests
-from urllib.parse import urlparse
+from urllib.parse import urlparse, ParseResult
 from tqdm import tqdm
 import ftplib
 import ftputil
@@ -103,6 +135,59 @@ class PolyReader:
             return line.decode()
         return line
 
+    def _wrap_ssh(self, parsed: ParseResult):
+        """
+        Wrap self._fh as needed.
+        """
+        # Handle SFTP URLs
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.connect(parsed.hostname, username=parsed.username, password=parsed.password)
+        self._sftp = client.open_sftp()
+        try:
+            self._sftp = self._sftp.file(parsed.path, 'rb')
+        except FileNotFoundError as err:
+            print(f"ERROR: No such file found: {self.filename}")
+            return exit(1), err
+        self._fh = self._sftp
+        if self.filename.endswith('.zst'):
+            dctx = zstd.ZstdDecompressor(max_window_size=2**31)
+            self._fh = io.TextIOWrapper(dctx.stream_reader(self._fh), encoding='utf-8')
+        elif self.filename.endswith('.bz2'):
+            self._fh = bz2.BZ2File(self._fh)
+        elif self.filename.endswith('.gz'):
+            self._fh = gzip.GzipFile(fileobj=self._fh)
+            pass
+        pass
+
+    def _wrap_http(self):
+        response = requests.get(self.filename, stream=True)
+        if self.filename.endswith(".zst"):
+            dctx = zstd.ZstdDecompressor(max_window_size=2**31)
+            self._fh = io.TextIOWrapper(dctx.stream_reader(response.content, read_across_frames=True), encoding='utf-8')
+        elif self.filename.endswith(".bz2"):
+            self._fh = bz2.open(response.raw, 'rt')
+        elif self.filename.endswith(".gz"):
+            self._fh = gzip.open(response.raw, 'rt')
+        else:
+            self._fh = response.iter_lines()
+            self._request_iterator = True  # Will need to decode the line since it will by bytes.
+            pass
+        pass
+
+    def _wrap_local(self):
+        if self.filename.endswith('.zst'):
+            dctx = zstd.ZstdDecompressor(max_window_size=2**31)
+            self._fh = io.TextIOWrapper(dctx.stream_reader(open(self.filename, 'rb')), encoding='utf-8')
+        elif self.filename.endswith('.bz2'):
+            self._fh = bz2.open(self.filename, 'rt')
+        elif self.filename.endswith('.gz'):
+            self._fh = gzip.open(self.filename, 'rt')
+        else:
+            self._fh = open(self.filename, 'r')
+            pass
+        pass
+
     def open(self):
         """
         Open the file for reading, decompressing it on the fly if necessary.
@@ -111,51 +196,11 @@ class PolyReader:
         parsed = urlparse(self.filename)
 
         if parsed.scheme in ('sftp', 'ssh'):
-            # Handle SFTP URLs
-            client = paramiko.SSHClient()
-            client.load_system_host_keys()
-            client.connect(parsed.hostname, username=parsed.username, password=parsed.password)
-            sftp = client.open_sftp()
-            try:
-                f = sftp.file(parsed.path, 'rb')
-            except FileNotFoundError as err:
-                print(f"ERROR: No such file found: {self.filename}")
-                return exit(1)
-            if self.filename.endswith('.zst'):
-                dctx = zstd.ZstdDecompressor(max_window_size=2**31)
-                self._fh = io.TextIOWrapper(dctx.stream_reader(f), encoding='utf-8')
-            elif self.filename.endswith('.bz2'):
-                self._fh = bz2.BZ2File(f)
-            elif self.filename.endswith('.gz'):
-                self._fh = gzip.GzipFile(fileobj=f)
-            else:
-                self._fh = f
-                pass
-            self._sftp = sftp
-        elif  parsed.scheme in ('http', 'https'):
-            response = requests.get(self.filename, stream=True)
-            if self.filename.endswith(".zst"):
-                dctx = zstd.ZstdDecompressor(max_window_size=2**31)
-                self._fh = io.TextIOWrapper(dctx.stream_reader(response.content, read_across_frames=True), encoding='utf-8')
-            elif self.filename.endswith(".bz2"):
-                self._fh = bz2.open(response.raw, 'rt')
-            elif self.filename.endswith(".gz"):
-                self._fh = gzip.open(response.raw, 'rt')
-            else:
-                self._fh = response.iter_lines()
-                self._request_iterator = True  # Will need to decode the line since it will by bytes.
-                pass
+            self._wrap_ssh(parsed)
+        elif parsed.scheme in ('http', 'https'):
+            self._wrap_http()
         else:
-            if self.filename.endswith('.zst'):
-                dctx = zstd.ZstdDecompressor(max_window_size=2**31)
-                self._fh = io.TextIOWrapper(dctx.stream_reader(open(self.filename, 'rb')), encoding='utf-8')
-            elif self.filename.endswith('.bz2'):
-                self._fh = bz2.open(self.filename, 'rt')
-            elif self.filename.endswith('.gz'):
-                self._fh = gzip.open(self.filename, 'rt')
-            else:
-                self._fh = open(self.filename, 'r')
-                pass
+            self._wrap_local()
             pass
 
         if self.show_progress:
@@ -243,6 +288,56 @@ class PolyWriter:
         self.close()
         pass
 
+    def _wrap_ftp(self, parsed: ParseResult):
+        self._ftp = ftputil.FTPHost(parsed.hostname, parsed.username, parsed.password, port=parsed.port,
+                                    session_factory=FTPSessionWrapper)
+        self.remote_filename = parsed.path
+        self._fh = self._ftp.open(parsed.path, 'wb')
+        if parsed.path.endswith('.zst'):
+            dctx = zstd.ZstdCompressor(level=22)
+            self._fh = io.TextIOWrapper(dctx.stream_writer(self._fh), encoding='utf-8')
+        elif self.filename.endswith('.bz2'):
+            self._fh = bz2.open(self._fh, compresslevel=9, mode='wt')
+        elif self.filename.endswith('.gz'):
+            self._fh = gzip.open(self._fh, compresslevel=9, mode='wt')
+            pass
+        pass
+
+    def _wrap_ssh(self, parsed: ParseResult):
+        # Handle SFTP URLs
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.connect(parsed.hostname, username=parsed.username, password=parsed.password)
+        sftp = client.open_sftp()
+        try:
+            self._fh = sftp.file(parsed.path, 'wt')
+        except FileNotFoundError as err:
+            print(f"ERROR: No such file found: {self.filename}")
+            return exit(1), err
+        if self.filename.endswith('.zst'):
+            dctx = zstd.ZstdCompressor(level=22)
+            self._fh = io.TextIOWrapper(dctx.stream_writer(self._fh), encoding='utf-8')
+        elif self.filename.endswith('.bz2'):
+            self._fh = bz2.open(self._fh, compresslevel=9, mode='wt')
+        elif self.filename.endswith('.gz'):
+            self._fh = gzip.open(self._fh, compresslevel=9, mode='wt')
+            pass
+        self._sftp = sftp
+        pass
+
+    def _wrap_local(self):
+        if self.filename.endswith('.zst'):
+            cctx = zstd.ZstdCompressor()
+            self._fh = io.TextIOWrapper(cctx.stream_writer(open(self.filename, 'wb')), encoding='utf-8')
+        elif self.filename.endswith('.bz2'):
+            self._fh = bz2.open(self.filename, 'wt')
+        elif self.filename.endswith('.gz'):
+            self._fh = gzip.open(self.filename, 'wt')
+        else:
+            self._fh = open(self.filename, 'w')
+            pass
+        pass
+
     def open(self):
         """
         Open the file for writing, compressing it on the fly if necessary.
@@ -250,60 +345,11 @@ class PolyWriter:
         parsed = urlparse(self.filename)
 
         if parsed.scheme == 'ftp':
-            print("FTP\n"
-                  f"  hostname={parsed.hostname}\n"
-                  f"  port={parsed.port}\n"
-                  f"  username={parsed.username}\n"
-                  f"  password={parsed.password}\n"
-                  f"  path={parsed.path}"
-            )
-            # self._ftp = FTP().connect(host=parsed.hostname, port=int(parsed.port))
-            # self._ftp.login(parsed.username, parsed.password)
-            # self.remote_filename = os.path.basename(parsed.path)
-            self._ftp = ftputil.FTPHost(parsed.hostname, parsed.username, parsed.password, port=parsed.port,
-                                        session_factory=FTPSessionWrapper)
-            self.remote_filename = parsed.path
-            self._fh = self._ftp.open(parsed.path, 'wb')
-            if parsed.path.endswith('.zst'):
-                dctx = zstd.ZstdCompressor(level=22)
-                self._fh = io.TextIOWrapper(dctx.stream_writer(self._fh), encoding='utf-8')
-            elif self.filename.endswith('.bz2'):
-                self._fh = bz2.open(self._fh, compresslevel=9, mode='wt')
-            elif self.filename.endswith('.gz'):
-                self._fh = gzip.open(self._fh, compresslevel=9, mode='wt')
-                pass
-            pass
+            self._wrap_ftp(parsed)
         elif parsed.scheme in ('sftp', 'ssh'):
-            # Handle SFTP URLs
-            client = paramiko.SSHClient()
-            client.load_system_host_keys()
-            client.connect(parsed.hostname, username=parsed.username, password=parsed.password)
-            sftp = client.open_sftp()
-            try:
-                self._fh = sftp.file(parsed.path, 'wt')
-            except FileNotFoundError as err:
-                print(f"ERROR: No such file found: {self.filename}")
-                return exit(1)
-            if self.filename.endswith('.zst'):
-                dctx = zstd.ZstdCompressor(level=22)
-                self._fh = io.TextIOWrapper(dctx.stream_writer(self._fh), encoding='utf-8')
-            elif self.filename.endswith('.bz2'):
-                self._fh = bz2.open(self._fh, compresslevel=9, mode='wt')
-            elif self.filename.endswith('.gz'):
-                self._fh = gzip.open(self._fh, compresslevel=9, mode='wt')
-                pass
-            self._sftp = sftp
+            self._wrap_ssh(parsed)
         else:
-            if self.filename.endswith('.zst'):
-                cctx = zstd.ZstdCompressor()
-                self._fh = io.TextIOWrapper(cctx.stream_writer(open(self.filename, 'wb')), encoding='utf-8')
-            elif self.filename.endswith('.bz2'):
-                self._fh = bz2.open(self.filename, 'wt')
-            elif self.filename.endswith('.gz'):
-                self._fh = gzip.open(self.filename, 'wt')
-            else:
-                self._fh = open(self.filename, 'w')
-                pass
+            self._wrap_local()
             pass
         pass
 
@@ -327,29 +373,24 @@ class PolyWriter:
 
     pass
 
-fmt = None
-def pinfo(preface: str, filename:str, line_count:int, bytes_count:int, seconds:float):
-    global fmt
-    if fmt is None:
-        from general.constants import CommonFormattingBase
-        fmt = CommonFormattingBase()
-        pass
-    rate = line_count/seconds
-    print(
-        f"{preface} {filename}. "
-        f"{line_count:,d} lines / {fmt.pbytes(bytes_count)} "
-        f"at {fmt.prate(rate)} ({fmt.pbyterate(bytes_count/seconds)}). "
-        f"Total time: {fmt.psecs(seconds)})."
-    )
-    pass
-
 
 def main():
     """
     Main function, mostly for testing.
     """
     import time
+    from general.constants import CommonFormattingBase
+    fmt = CommonFormattingBase()
 
+    def pinfo(preface: str, filename: str, line_count: int, bytes_count: int, seconds: float, fmt: 'CommonFormattingBase'):
+        rate = line_count / seconds
+        print(
+            f"{preface} {filename}. "
+            f"{line_count:,d} lines / {fmt.pbytes(bytes_count)} "
+            f"at {fmt.prate(rate)} ({fmt.pbyterate(bytes_count/seconds)}). "
+            f"Total time: {fmt.psecs(seconds)})."
+        )
+        pass
     parser = argparse.ArgumentParser(description='Test reading of plain text, gzipped, bzip2, or zstandard compressed files locally and remotely.')
     parser.add_argument('files', type=str, nargs='*', default=[], help='The input file(s) to process', metavar="file")
     group = parser.add_mutually_exclusive_group(required=True)
@@ -364,9 +405,6 @@ def main():
         exit(1)
         pass
 
-    # if not args.input_file and not args.output_file:
-    #     return parser.error("At least one of --input-file or --output-file are required.")
-
     if args.read:
         for input_file in args.files:
             print(f"TESTING reading {input_file}")
@@ -379,7 +417,7 @@ def main():
                     bytes_count += len(line)
                     pass
                 # print(f"Last line: {line}")
-                pinfo("FINISHED Reading.", reader.filename, line_count, bytes_count, time.monotonic() - t0)
+                pinfo("FINISHED Reading.", reader.filename, line_count, bytes_count, time.monotonic() - t0, fmt)
                 pass
             pass
         pass
@@ -397,12 +435,13 @@ def main():
                         bytes_count += len(line)
                         writer.write(line)
                         pass
-                    pinfo("FINISHED Writing.", writer.filename, line_count, bytes_count, time.monotonic() - t0)
+                    pinfo("FINISHED Writing.", writer.filename, line_count, bytes_count, time.monotonic() - t0, fmt)
                     pass
                 pass
             pass
         pass
     pass
+
 
 if __name__ == "__main__":
     main()
